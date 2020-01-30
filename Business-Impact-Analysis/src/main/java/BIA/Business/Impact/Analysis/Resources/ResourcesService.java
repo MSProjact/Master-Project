415fa795c4145213c17853e01bcdf4cb36af3196
HP <HP@DESKTOP-GH9EJH5.fritz.box>
2020-01-30 12:53:43 +0100

    Employees and Financial Resources Classes

---
+++ Business-Impact-Analysis/src/main/java/BIA/Business/Impact/Analysis/Resources/ResourcesService.java
@@ -0,0 +1,34 @@
+package BIA.Business.Impact.Analysis.Resources;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import BIA.Business.Impact.Analysis.EmployeesRepository;
+@Service
+@Transactional
+public class ResourcesService {
+	
+	 @Autowired
+	    private ResourcesRepository repo;
+	     
+	    public List<Resources> listAll() {
+	        return repo.findAll();
+	    }
+	     
+	    public void save(Resources Resources) {
+	        repo.save(Resources);
+	    }
+	     
+	    public Resources get(int id) {
+	        return repo.findById(id).get();
+	    }
+	     
+	    public void delete(int id) {
+	        repo.deleteById(id);
+	    }
+}
+
