7c5180b0d342fc55891c0b589391146798a4fa80
Sharjeel <Sharjeel@DESKTOP-UJFP3IS>
2020-02-01 18:34:20 +0100

    Products and Production Steps

---
+++ Business-Impact-Analysis/src/main/java/BIA/Business/Impact/Analysis/ProductionSteps/ProductionStepsController.java
@@ -0,0 +1,63 @@
+package BIA.Business.Impact.Analysis.ProductionSteps;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import BIA.Business.Impact.Analysis.ProductionSteps.ProductionSteps;
+import BIA.Business.Impact.Analysis.ProductionSteps.ProductionStepsService;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.Model;
+
+@Controller("/ProductionSteps")
+public class ProductionStepsController {
+
+	@Autowired
+	private ProductionStepsService service;
+
+	@RequestMapping("/ProductionStepslist")
+	public String viewHomePage(Model model) {
+		List<ProductionSteps> ProductionStepslist = service.listAll();
+		model.addAttribute("ProductionStepslist", ProductionStepslist);
+		return "index_ProductionSteps";
+
+	}
+
+	@RequestMapping("/newProductionStep")
+	public String showNewProductionStepsPage(Model model) {
+		ProductionSteps ProductionSteps = new ProductionSteps();
+		model.addAttribute("ProductionSteps", ProductionSteps);
+
+		return "Add_NewProductionSteps";
+	}
+
+	@RequestMapping(value = "/saveProductionStep", method = RequestMethod.POST)
+	public String saveProduct(@ModelAttribute("ProductionSteps") ProductionSteps ProductionSteps) {
+		service.save(ProductionSteps);
+
+		return "redirect:/ProductionStepslist";
+	}
+
+	
+	  @RequestMapping("/editProductionStep/{id}") public ModelAndView
+	  showEditProductPage(@PathVariable(name = "id") int id) { ModelAndView mav =
+	  new ModelAndView("Edit_ProductionSteps"); ProductionSteps ProductionSteps = service.get(id);
+	  mav.addObject("ProductionSteps", ProductionSteps);
+	  
+	 return mav; }
+
+	 
+	  @RequestMapping("/deleteProductionStep/{id}")
+	  public String deleteProductionSteps(@PathVariable(name = "id") int id) {
+	      service.delete(id);
+	      return "redirect:/ProductionStepslist";       
+	  }
+
+}
