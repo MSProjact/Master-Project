3316ccad76945035733bc83c8e2108a5302a8e6c
arshadmuhammadsharjeel <sharjeelarshad4@gmail.com>
2020-02-27 20:16:58 +0100

    Tree Hierarchy Display with Dashboard
    Co-authored-by: Yasir-Abbasi <yasirabbasi.se@gmail.com>

--- Business-Impact-Analysis/src/main/java/BIA/Business/Impact/Analysis/Controller/ProductsController.java
+++ Business-Impact-Analysis/src/main/java/BIA/Business/Impact/Analysis/Controller/ProductsController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -26,38 +25,33 @@ public class ProductsController {
 	public String viewHomePage(Model model) {
 		List<Products> Productslist = service.listAll();
 		model.addAttribute("Productslist", Productslist);
-		return "index_Products";
-
+		return "Manage_Products";
 	}
 
 	@RequestMapping("/newProduct")
 	public String showNewProductsPage(Model model) {
 		Products Products = new Products();
 		model.addAttribute("Products", Products);
-
 		return "Add_NewProduct";
 	}
 
 	@RequestMapping(value = "/saveProduct", method = RequestMethod.POST)
 	public String saveProduct(@ModelAttribute("Products") Products Products) {
 		service.save(Products);
-
 		return "redirect:/Productslist";
 	}
 
-	
-	  @RequestMapping("/editproduct/{id}") public ModelAndView
-	  showEditProductPage(@PathVariable(name = "id") int id) { ModelAndView mav =
-	  new ModelAndView("Edit_Products"); Products Products = service.get(id);
-	  mav.addObject("Products", Products);
-	  
-	 return mav; }
+	@RequestMapping("/editproduct/{id}")
+	public ModelAndView showEditProductPage(@PathVariable(name = "id") int id) {
+		ModelAndView mav = new ModelAndView("Edit_Products");
+		Products Products = service.get(id);
+		mav.addObject("Products", Products);
+		return mav;
+	}
 
-	 
-	  @RequestMapping("/deleteProduct/{id}")
-	  public String deleteProducts(@PathVariable(name = "id") int id) {
-	      service.delete(id);
-	      return "redirect:/Productslist";       
-	  }
-
+	@RequestMapping("/deleteProduct/{id}")
+	public String deleteProducts(@PathVariable(name = "id") int id) {
+		service.delete(id);
+		return "redirect:/Productslist";
+	}
 }
--- Business-Impact-Analysis/src/main/java/BIA/Business/Impact/Analysis/Repository/EmployeesRepository.java
+++ Business-Impact-Analysis/src/main/java/BIA/Business/Impact/Analysis/Repository/EmployeesRepository.java
@@ -1,16 +1,10 @@
 package BIA.Business.Impact.Analysis.Repository;
 
-import java.util.Optional;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
-
 import BIA.Business.Impact.Analysis.Model.Employees;
-import BIA.Business.Impact.Analysis.Model.GenerateHierarchy;
 
-public interface EmployeesRepository  extends  MongoRepository <Employees, Integer> {
-
-	
+public interface EmployeesRepository extends MongoRepository<Employees, Integer> {
 
 }
+
